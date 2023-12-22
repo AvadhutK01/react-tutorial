55dd0bc71d3114696769eb349f2a3b51ebdd8506
AvadhutK01 <kelaskaravadhut052@gmail.com>
2023-12-23 00:18:49 +0530

    Embedding js variables in ui

--- react-tutorial/src/components/ExpenseItem.jsx
+++ react-tutorial/src/components/ExpenseItem.jsx
@@ -1,28 +1,40 @@
 import React from 'react'
+import './ExpenseItem.css'
 
 const ExpenseItem = () => {
+    const expenseDate = new Date(2023, 23, 12);
+    const LocationOfExpenditure = 'Shriwardhan';
     return (
-        <div>
-            <div>Expense Items</div>
-            <table border="1">
+        <table border="1" className='expense-table'>
+            <thead>
                 <tr>
+                    <th>Date</th>
                     <th>Description</th>
                     <th>Amount</th>
+                    <th>Location</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td className='expense-item'>{expenseDate.toDateString()}</td>
+                    <td >Food</td>
+                    <td className='expense-item__price'>Rs 10</td>
+                    <td>{LocationOfExpenditure}</td>
                 </tr>
                 <tr>
-                    <td>Food</td>
-                    <td>Rs 10</td>
-                </tr>
-                <tr>
+                    <td className='expense-item'>{expenseDate.toDateString()}</td>
                     <td>Petrol</td>
-                    <td>Rs 100</td>
+                    <td className='expense-item__price'>Rs 100</td>
+                    <td>{LocationOfExpenditure}</td>
                 </tr>
                 <tr>
+                    <td className='expense-item'>{expenseDate.toDateString()}</td>
                     <td>Movies</td>
-                    <td>Rs 200</td>
+                    <td className='expense-item__price'>Rs 200</td>
+                    <td>{LocationOfExpenditure}</td>
                 </tr>
-            </table>
-        </div>
+            </tbody>
+        </table>
     )
 }
 
