19131c1ae94a8d5321a28ba30e90b3deef30fc90
AvadhutK01 <kelaskaravadhut052@gmail.com>
2023-12-27 00:37:52 +0530

    Using props and creating reusable components (primary stage)

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,11 +1,93 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  const expenses = [
+    {
+      id: 1,
+      date: new Date(2023, 1, 1),
+      location: "Mumbai",
+      description: "Tea",
+      amount: 150,
+    },
+    {
+      id: 2,
+      date: new Date(2023, 1, 2),
+      location: "Delhi",
+      description: "Lunch",
+      amount: 200,
+    },
+    {
+      id: 3,
+      date: new Date(2023, 1, 3),
+      location: "Bangalore",
+      description: "Dinner",
+      amount: 250,
+    },
+    {
+      id: 4,
+      date: new Date(2023, 1, 4),
+      location: "Chennai",
+      description: "Snacks",
+      amount: 120,
+    },
+    {
+      id: 5,
+      date: new Date(2023, 1, 5),
+      location: "Hyderabad",
+      description: "Coffee",
+      amount: 100,
+    },
+    {
+      id: 6,
+      date: new Date(2023, 1, 6),
+      location: "Kolkata",
+      description: "Breakfast",
+      amount: 180,
+    },
+    {
+      id: 7,
+      date: new Date(2023, 1, 7),
+      location: "Pune",
+      description: "Lunch",
+      amount: 200,
+    },
+    {
+      id: 8,
+      date: new Date(2023, 1, 8),
+      location: "Ahmedabad",
+      description: "Dinner",
+      amount: 220,
+    },
+    {
+      id: 9,
+      date: new Date(2023, 1, 9),
+      location: "Jaipur",
+      description: "Snacks",
+      amount: 130,
+    },
+    {
+      id: 10,
+      date: new Date(2023, 1, 10),
+      location: "Lucknow",
+      description: "Tea",
+      amount: 160,
+    }
+  ];
+
   return (
     <div>
       <h1>Expense Tracker</h1>
-      <ExpenseItem></ExpenseItem>
-    </div>
+      {Array.from({ length: 10 }, (_, index) => (
+        <ExpenseItem key={expenses[index].id}
+          date={expenses[index].date}
+          location={expenses[index].location}
+          description={expenses[index].description}
+          amount={expenses[index].amount}
+        ></ExpenseItem>
+      ))
+      }
+    </div >
   );
 }
 
--- react-tutorial/src/components/ExpenseItem.jsx
+++ react-tutorial/src/components/ExpenseItem.jsx
@@ -1,40 +1,17 @@
 import React from 'react'
 import './ExpenseItem.css'
 
-const ExpenseItem = () => {
-    const expenseDate = new Date(2023, 23, 12);
-    const LocationOfExpenditure = 'Shriwardhan';
+const ExpenseItem = (props) => {
     return (
-        <table border="1" className='expense-table'>
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Description</th>
-                    <th>Amount</th>
-                    <th>Location</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td className='expense-item'>{expenseDate.toDateString()}</td>
-                    <td >Food</td>
-                    <td className='expense-item__price'>Rs 10</td>
-                    <td>{LocationOfExpenditure}</td>
-                </tr>
-                <tr>
-                    <td className='expense-item'>{expenseDate.toDateString()}</td>
-                    <td>Petrol</td>
-                    <td className='expense-item__price'>Rs 100</td>
-                    <td>{LocationOfExpenditure}</td>
-                </tr>
-                <tr>
-                    <td className='expense-item'>{expenseDate.toDateString()}</td>
-                    <td>Movies</td>
-                    <td className='expense-item__price'>Rs 200</td>
-                    <td>{LocationOfExpenditure}</td>
-                </tr>
-            </tbody>
-        </table>
+        <div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+            <div style={{ 'marginLeft': '10px' }}>{props.location}</div>
+            <div className='expense-item__description'>
+                <h2>{props.description}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </div>
+
     )
 }
 
