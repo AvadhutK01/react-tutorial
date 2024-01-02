1a9a7c4f867d7009c7647f39dc6925620e82eee4
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-01-02 23:49:51 +0530

    Added dummy filter component

--- react-tutorial/src/components/Expenses/ExpenseItem.jsx
+++ react-tutorial/src/components/Expenses/ExpenseItem.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Expenses.css'
 import ExpenseDetails from './ExpenseDetails'
 import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
 
 const ExpenseItem = (props) => {
+    const [filterdYear, setFilterdYear] = useState(new Date().getFullYear());
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilterdYear(selectedYear);
+    }
     return (
         <Card className="expenses">
             {props.items.map((expense) => (
-                <ExpenseDetails
-                    key={expense.id}
-                    date={expense.date}
-                    amount={expense.amount}
-                    location={expense.location}
-                    description={expense.description} />
+                <>
+                    <ExpensesFilter selected={filterdYear} onChange={filterChangeHandler} />
+                    <ExpenseDetails
+                        key={expense.id}
+                        date={expense.date}
+                        amount={expense.amount}
+                        location={expense.location}
+                        description={expense.description} />
+                </>
             ))}
         </Card>
 
