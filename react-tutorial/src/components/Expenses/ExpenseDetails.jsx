d3205aa34a3f6f221469cabec8a4ee90dc0657f0
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-01-02 19:30:25 +0530

    Dom manpulation event listeners

--- react-tutorial/src/components/Expenses/ExpenseDetails.jsx
+++ react-tutorial/src/components/Expenses/ExpenseDetails.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseItem.css'
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate'
 
 
 const ExpenseDetails = (props) => {
+    let [title, setTitle] = useState(props.description);
+    const changeTitleHandler = () => {
+        setTitle('Changed Title');
+    }
+    const deleteHandler = () => {
+
+    }
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
@@ -12,11 +19,13 @@ const ExpenseDetails = (props) => {
                 {props.location}
             </div>
             <div className='expense-item__description'>
-                <h2>{props.description}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>
                     ₹ {props.amount}
                 </div>
             </div>
+            <button onClick={changeTitleHandler}>Change Title</button>
+            <button onClick={deleteHandler}>Delete</button>
         </Card>
     )
 }
