652dded8068437c23b760cc33e0ee644275fd495
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-01-02 20:37:55 +0530

    Using states to store input values

--- react-tutorial/src/components/Expenses/ExpenseForm.jsx
+++ react-tutorial/src/components/Expenses/ExpenseForm.jsx
@@ -1,43 +1,54 @@
 import React, { useState } from 'react'
+import './ExpenseForm.css'
 
 const ExpenseForm = () => {
-    const [amount, setAmount] = useState('');
-    const [date, setDate] = useState('');
-    const [location, setLocation] = useState('');
-    const [description, setDescription] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    const [enteredLocation, setEnteredLocation] = useState('');
+    const [enteredtitle, setEnteredtitle] = useState('');
 
     const submitHandler = (e) => {
         e.preventDefault();
         const expenseData = {
-            amount: amount,
-            date: date,
-            location: location,
-            description: description
+            amount: enteredAmount,
+            date: enteredDate,
+            location: enteredLocation,
+            description: enteredtitle,
         }
         console.log(expenseData);
     }
 
     return (
-        <div>
-            <h1>Expense Form</h1>
-            <form>
-                <label>
-                    Date:
-                </label>
-                <input type="date" onChange={(e) => setDate(e.target.value)} />
-                <br />
-                <label>Amount:</label>
-                <input type="number" onChange={(e) => setAmount(e.target.value)} />
-                <br />
-                <label>Location:</label>
-                <input type="text" onChange={(e) => setLocation(e.target.value)} />
-                <br />
-                <label>Description:</label>
-                <input type="text" onChange={(e) => setDescription(e.target.value)} />
-                <br />
-                <button onClick={submitHandler}>Add Expense</button>
-            </form>
-        </div>
+        <form onSubmit={submitHandler}>
+            <div className="new-expense__controls">
+                <h1>Expense Form</h1>
+                <div className="new-expense__controls">
+                    <label>
+                        Date:
+                    </label>
+                    <input type="date" onChange={(e) => setEnteredDate(e.target.value)} />
+                    <br />
+                </div>
+                <div className="new-expense__controls">
+                    <label>Amount:</label>
+                    <input type="number" onChange={(e) => setEnteredAmount(e.target.value)} />
+                    <br />
+                </div>
+                <div className="new-expense__controls">
+                    <label>Location:</label>
+                    <input type="text" onChange={(e) => setEnteredLocation(e.target.value)} />
+                    <br />
+                </div>
+                <div className="new-expense__controls">
+                    <label>Title:</label>
+                    <input type="text" onChange={(e) => setEnteredtitle(e.target.value)} />
+                    <br />
+                </div>
+                <div className='new-expense__actions'>
+                    <button type='submit'>Add Expense</button>
+                </div>
+            </div>
+        </form>
     )
 }
 
