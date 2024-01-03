d4c49ac80d47023eae71618c2f51b7cbe9d2e57e
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-01-03 19:20:33 +0530

    conditional rendering practice

--- react-tutorial/src/components/Expenses/ExpenseForm.jsx
+++ react-tutorial/src/components/Expenses/ExpenseForm.jsx
@@ -6,6 +6,7 @@ const ExpenseForm = (props) => {
     const [enteredDate, setEnteredDate] = useState('');
     const [enteredLocation, setEnteredLocation] = useState('');
     const [enteredTitle, setEnteredTitle] = useState('');
+    const [isVisible, setIsVisible] = useState(false);
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -20,38 +21,50 @@ const ExpenseForm = (props) => {
         setEnteredAmount('');
         setEnteredLocation('');
         setEnteredTitle('');
+        setIsVisible(false);
     }
 
     return (
-        <form onSubmit={submitHandler}>
-            <div className="new-expense__controls">
+        isVisible ? (
+            <form onSubmit={submitHandler}>
                 <div className="new-expense__controls">
-                    <label>
-                        Date:
-                    </label>
-                    <input type="date" value={enteredDate} onChange={(e) => setEnteredDate(e.target.value)} />
-                    <br />
+                    <div className="new-expense__controls">
+                        <label>
+                            Date:
+                        </label>
+                        <input type="date" value={enteredDate} onChange={(e) => setEnteredDate(e.target.value)} />
+                        <br />
+                    </div>
+                    <div className="new-expense__controls">
+                        <label>Amount:</label>
+                        <input type="number" value={enteredAmount} onChange={(e) => setEnteredAmount(e.target.value)} />
+                        <br />
+                    </div>
+                    <div className="new-expense__controls">
+                        <label>Location:</label>
+                        <input type="text" value={enteredLocation} onChange={(e) => setEnteredLocation(e.target.value)} />
+                        <br />
+                    </div>
+                    <div className="new-expense__controls">
+                        <label>Title:</label>
+                        <input type="text" value={enteredTitle} onChange={(e) => setEnteredTitle(e.target.value)} />
+                        <br />
+                    </div>
+                    <div className='new-expense__actions'>
+                        <button type='submit'>Add Expense</button>
+                    </div>
+                    <div className='new-expense__actions'>
+                        <button type='submit' onClick={() => setIsVisible(false)}>Cancel</button>
+                    </div>
                 </div>
-                <div className="new-expense__controls">
-                    <label>Amount:</label>
-                    <input type="number" value={enteredAmount} onChange={(e) => setEnteredAmount(e.target.value)} />
-                    <br />
-                </div>
-                <div className="new-expense__controls">
-                    <label>Location:</label>
-                    <input type="text" value={enteredLocation} onChange={(e) => setEnteredLocation(e.target.value)} />
-                    <br />
-                </div>
-                <div className="new-expense__controls">
-                    <label>Title:</label>
-                    <input type="text" value={enteredTitle} onChange={(e) => setEnteredTitle(e.target.value)} />
-                    <br />
-                </div>
+            </form>
+        ) : (
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <div className='new-expense__actions'>
-                    <button type='submit'>Add Expense</button>
+                    <button type='submit' onClick={() => setIsVisible(true)}>Add new Expense</button>
                 </div>
             </div>
-        </form>
+        )
     )
 }
 
