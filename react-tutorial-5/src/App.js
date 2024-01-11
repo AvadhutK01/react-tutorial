9b0563200f209e5042a6ce0c3c4319a8c4592043
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-01-11 13:24:50 +0530

    some behind the scenes

--- react-tutorial-5/src/App.js
+++ react-tutorial-5/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
+import DemoComponent from './components/Demo/DemoComponent';
 
 function App() {
+  const [isTrue, setIsTrue] = useState(false);
+
+  const setTrue = () => {
+    setIsTrue(prev => !prev)
+  }
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoComponent show={isTrue} />
+      <Button type='submit' onClick={setTrue}>Set true</Button>
     </div>
   );
 }
---
+++ react-tutorial-5/src/components/Demo/DemoComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DemoComponent = (props) => {
+    return (
+        <p>{props.show ? "This is a simple react app." : ""}</p>
+    )
+}
+
+export default DemoComponent
